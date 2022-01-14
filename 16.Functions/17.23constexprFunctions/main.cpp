25546bfca1d4348e82f0616775cc02ad85e995ad
Daniel Gakwaya <wayadn88@gmail.com>
2022-01-14 09:49:52 +0200

    Fix the lecture on constexpr functions

--- 16.Functions/17.23constexprFunctions/main.cpp
+++ 16.Functions/17.23constexprFunctions/main.cpp
@@ -9,12 +9,20 @@ constexpr int get_value(int multiplier){
 
 int main(){
 
-    //int result = get_value(4); // Compile time
-    //std::cout << "result : " << result << std::endl;
+    /*
+        For the evaluation to take place at compile time, we have 
+        to remember to store the return value in a constexpr result variable
+        Thanks to Reza for catching this error and reporting it. 
 
+    */
+    constexpr int result = get_value(4); // Compile time
+    std::cout << "result : " << result << std::endl;
+
+    /*
     int some_var{5}; // Run time variable
     int result = get_value(some_var); // Run time
     std::cout << "result : " << result << std::endl;
+    */
    
     return 0;
 }
