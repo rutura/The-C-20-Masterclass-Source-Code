7ec32659e00b3bd2e8e6ba857ff233590991e7c6
mdsiaofficial <mdsiaofficial@gmail.com>
2023-03-01 12:16:35 +0600

    b

--- 21.LambdaFunctions/21.2DeclaringAndUsingLambdas/new.cpp
+++ 21.LambdaFunctions/21.2DeclaringAndUsingLambdas/new.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
 using namespace std;
 int main(){
+
     auto func = [](){
         cout<<"Hello World!"<<endl;
     };
 
     func();
+    
+    [](){
+        cout<<"Hello World!"<<endl;
+    }();
+
+
+
 }
