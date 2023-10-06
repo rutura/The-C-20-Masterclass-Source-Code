0e0fa81039ca3cf2d9f38e26f859dfe80b10060f
Daniel Gakwaya <wayadn88@gmail.com>
2023-10-06 09:33:32 +0200

    Add code for lectures on templates, namespaces and visibility

---
+++ 49.Modules/04.ALL_COMPILERS/48.11BoxContainer/main.cpp
@@ -0,0 +1,25 @@
+// 48.11BoxContainer.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <algorithm>
+import <iostream>;
+import BoxContainer;
+
+int main()
+{
+    BoxContainer<int> box1;
+    box1.add(5);
+    box1.add(1);
+    box1.add(4);
+    box1.add(8);
+    box1.add(5);
+    box1.add(3);
+    box1.add(7);
+    box1.add(9);
+    box1.add(6);
+
+    std::cout << "box1 : " << box1 << std::endl;
+    std::ranges::sort(box1.begin(), box1.end());
+    std::cout << "box1 : " << box1 << std::endl;
+}
+
---
+++ 49.Modules/04.ALL_COMPILERS/48.13VisibilityReachability/main.cpp
@@ -0,0 +1,13 @@
+import math;
+import <iostream>;
+
+int main()
+{
+    //Point is not visible. We can't create its instance
+    //math::Point p(1, 2);
+
+    auto p = math::generateRandomPoint();
+    //print p
+    std::cout << p << std::endl;
+    std::cout << "x: " << p.getX() << std::endl;
+}
