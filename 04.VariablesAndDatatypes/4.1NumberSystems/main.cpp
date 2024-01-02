bb67de4f37e97727cd0ecb694f02d28cc4a8b438
Daniel Gakwaya <wayadn88@gmail.com>
2024-01-03 00:13:51 +0200

    Initial stages of updates to chapter 4

---
+++ 04.VariablesAndDatatypes/4.1NumberSystems/main.cpp
@@ -0,0 +1,28 @@
+/*
+    Number Systems
+          - Integers - OK
+          - Floating points: IEEE 754
+          - Numeric limits: How to show the ranges
+*/
+
+#include <fmt/format.h>
+
+int main(){
+
+   // Integers
+   int number1 = 15; // Decimal
+   int number2 = 017; // Octal
+   int number3 = 0x0F; // Hexadecimal
+   int number4 = 0b00001111; // Binary
+
+   fmt::println("number1 : {}", number1);
+   fmt::println("number2 : {}", number2);
+   fmt::println("number3 : {}", number3);
+   fmt::println("number4 : {}", number4);
+
+   // Floating points
+   //The sky 
+
+   
+    return 0;
+}
