581e8ba314c9c8a1fc2df6a0c041d70a4c18d4da
Koech <koech@novek.io>
2024-03-23 12:27:56 +0300

    (cpp23-update-cmake-and_streams-update): updated output stream to fmt's

--- 08.BitwiseOperators/8.2PrintingIntegersInBinary/main.cpp
+++ 08.BitwiseOperators/8.2PrintingIntegersInBinary/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <fmt/format.h>
 #include <bitset>
 
 
@@ -6,10 +6,11 @@ int main(){
 
     unsigned short int data {15};
 
-    std::cout << "data (dec) : " <<std::showbase <<  std::dec << data << std::endl;
-    std::cout << "data (oct) : " <<std::showbase <<  std::oct << data << std::endl;
-    std::cout << "data (hex) : " <<std::showbase <<  std::hex << data << std::endl;
-    std::cout << "data (bin) : " << std::bitset<16>(data) << std::endl;
-   
+    fmt::println("data (dec) : {0:#d}",  data );
+    fmt::println("data (oct) : {0:#o}" ,  data );
+    fmt::println("data (hex) : {0:#x}",data );
+    fmt::println("data (bin) : {0:#b}",  data );
+    fmt::println("data (bin) : {}",  std::bitset<16>(data).to_string() );
+
     return 0;
 }
