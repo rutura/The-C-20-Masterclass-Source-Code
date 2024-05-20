cc3128d93234da1ec242a308cc00ead5d359bf0c
Daniel Gakwaya <wayadn88@gmail.com>
2024-05-20 09:39:02 +0300

    Add instructions on possible errors to the project

--- 03.FirstSteps/3.4GUIChallenge/main.cpp
+++ 03.FirstSteps/3.4GUIChallenge/main.cpp
@@ -4,6 +4,14 @@
         . Show a circle in the window
         . Change the color of the circle
         . Change the color of the background
+        . On linux, I had a few issues may be related to missing binaries on the system.
+          . Installed these and the issue seemed to go away: 
+              . sudo apt-get install libx11-dev libxrandr-dev libxcursor-dev libxi-dev libudev-dev libgl1-mesa-dev
+              . splitting the target_link_libraries command into two also seemd to change something: 
+                  . target_link_libraries(rooster PRIVATE fmt::fmt) 
+                  . target_link_libraries(rooster PRIVATE sfml-graphics)
+              . Improvise on these problems.
+                   
 
 */
 
