7f2ff49e9f1f287d71c7f2992ed96a7973184795
Daniel Gakwaya <wayadn88@gmail.com>
2024-05-20 10:07:44 +0300

    Add futher instructions on the toolset for modules

--- 03.FirstSteps/3.4GUIChallenge/main.cpp
+++ 03.FirstSteps/3.4GUIChallenge/main.cpp
@@ -5,6 +5,17 @@
         . Change the color of the circle
         . Change the color of the background
 
+    . NOTE: 
+        . This example won't work in containers because they don't have a xserver
+            set up. It's possible to set up one but we'll pass for this course.
+        . You can run it: 
+            . On a local box with the requirements met: 
+                . a compiler with support for modules with cmake:   
+                    . gcc 14 and up
+                    . clang 16 and up
+                    . latest visual c++
+        . In this course, we will run it on windows 
+
 */
 
 #include <SFML/Graphics.hpp>
