865ae4e1995624f936037ca97df4758a777135af
Daniel Gakwaya <wayadn88@gmail.com>
2024-05-28 12:50:58 +0200

    add placeholder for challenge in chapter on flow control

---
+++ 06.FlowControl/6.3Challenge/main.cpp
@@ -0,0 +1,28 @@
+/*
+    . GUI Challenge: SFML
+        . Change the application to store data in predefined variables.
+
+    . NOTE: 
+        . This example won't work in containers because they don't have a xserver
+            set up. It's possible to set up one but we'll pass for this course.
+        . You can run it: 
+            . On a local box with the requirements met: 
+                . a compiler with support for modules with cmake:   
+                    . gcc 14 and up
+                    . clang 16 and up
+                    . latest visual c++
+        . Be carefult about the opengl dependencies that vcpkg will complain about in the output .
+        . Note that even if we don't have access to xserver we can still build the 
+            binary successfuly. We simply can't run it and see a GUI.
+        . In this course, we will run it on windows 
+
+*/
+import utilities;
+
+int main()
+{
+  print_msg("starting out...");
+  app();
+
+  return 0;
+}
