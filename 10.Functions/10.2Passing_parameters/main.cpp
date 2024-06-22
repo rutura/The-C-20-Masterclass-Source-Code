0bd13bad3cb089643a4d61d282c88d7582f88ba1
Daniel Gakwaya <wayadn88@gmail.com>
2024-06-22 18:12:31 +0200

    add nugget about default function parameters

--- 10.Functions/10.2Passing_parameters/main.cpp
+++ 10.Functions/10.2Passing_parameters/main.cpp
@@ -111,10 +111,33 @@ void say_age(int &age)
 */
 
 //Pass by const reference
+/*
 void say_age(const int &age)
 {
   //++age;
   fmt::println("Hello, you are {} years old! &age: {}", age, fmt::ptr(&age));// 24
+}
+*/
+
+//Default parameters: They only have to show up in the declaration.
+void compute(int age = 27, double weight = 72.4, double distance = 12.5)
+{
+  fmt::println("Doing computations on age : {} weight :{} and distance :{}  ", age, weight, distance);
+}
+
+void greet_teacher(
+        std::string_view name_sv = "teacher",
+        int homeworks = 12,
+        int exams = 4,
+        double pass_rate = 0.5,
+        std::string_view first_dpmt = "Computer Sce")
+{
+  fmt::println("Good morning {} !", name_sv);
+  fmt::println("In the past semester, we had {} homeworks, and {} exams. The pass rate was around {} ",
+    homeworks,
+    exams,
+    pass_rate);
+  fmt::println("The best performing department is {} ", first_dpmt);
 }
 
 int main() {
@@ -170,9 +193,22 @@ int main() {
     */
 
     //Pass by const reference
+    /*
     int age{ 23 };// Local
     fmt::println("age (before call) : {} &age : {}", age, fmt::ptr(&age));
     say_age(age);// Argument
     fmt::println("age (after call) : {} &age : {}", age, fmt::ptr(&age));
+    */
+
+    //Default parameters
+    //compute();
+
+    //Call and use default arguments
+    greet_teacher();
+    greet_teacher("Mr Bean");
+    greet_teacher("Mr Hamston", 7);
+    greet_teacher("Mr Walker", 7, 3);
+    greet_teacher("Mr Paku", 7, 3, 0.7);
+    greet_teacher("Mr Kojo", 7, 3, 0.7, "Applied Mathematics");
 
 }
