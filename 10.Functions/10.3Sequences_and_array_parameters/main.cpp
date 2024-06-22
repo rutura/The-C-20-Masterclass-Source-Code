465e9bec9676d7bcd80b0756fa752c01e19748ed
Daniel Gakwaya <wayadn88@gmail.com>
2024-06-22 18:03:25 +0200

    factor in multi dimensional array function parameters

--- 10.Functions/10.3Sequences_and_array_parameters/main.cpp
+++ 10.Functions/10.3Sequences_and_array_parameters/main.cpp
@@ -80,14 +80,49 @@ double sum(const double (&scores)[10])
 }
 */
 
+// Passing multi-dimensional arrays as function parameters (2d and 3d)
+double sum(const double array[][3], size_t size)
+{
+  double sum{};
+  for (size_t i{}; i < size; ++i)// Loop through rows
+  {
+    for (size_t j{}; j < 3; ++j)// Loop through elements in a row
+    {
+      sum += array[i][j];// Array access notation
+      // sum += *( *(array + i) +j);	// Pointer arithmetic notation.
+      // Confusing . Prefer array noation
+    }
+  }
+  return sum;
+}
 
-//Create a function that takes an std::vector and returns the sum of the elements
+double sum_3d(const double array[][3][2], size_t size)
+{
+
+  double sum{};
+  for (size_t i{}; i < size; ++i)// Loop through rows
+  {
+    for (size_t j{}; j < 3; ++j)// Loop through elements in a row
+    {
+      for (size_t k{}; k < 2; ++k) {
+        sum += array[i][j][k];
+        // sum += *(*(*(array + i) + j)+k);
+      }
+    }
+  }
+  return sum;
+}
+
+
+//Passing std::array as a function parameter
+/*
 double sum(const std::array<double, 10>& scores)
 {
   double sum{};
   for (size_t i{}; i < scores.size(); ++i) { sum += scores[i]; }
   return sum;
 }
+*/
 
 
 int main() {
@@ -119,10 +154,40 @@ int main() {
     fmt::println("result is : {}", sum_result);
     */
 
-    //Create a function that takes an std::vector and returns the sum of the elements
+    //Passing multi-dimensional arrays as function parameters (2d and 3d)
+    double weights[][3]{
+        { 10.0, 20.0,30.0},
+        { 40.0, 50.0, 60.0 },
+        { 70.0, 80.0, 90.0 },
+        { 100.0, 110.0, 120.0 } 
+    };
+
+    double weights_3d[][3][2]{
+        {
+            { 10, 20 },
+            { 30, 40 },
+            { 50, 60 },
+        },
+        {
+            { 70, 80 },
+            { 90, 100 },
+            { 110, 120 },
+        } 
+    };
+
+    double result = sum(weights, std::size(weights));
+    fmt::println("2d array sum: {}", result);
+
+
+    result = sum_3d(weights_3d, std::size(weights_3d));
+    fmt::println("3d array sum: {}", result);
+
+    //Passing std::array as a function parameter
+    /*
     std::array<double, 10> student_scores{ 10.0, 20.0, 30.0};
     double sum_result = sum(student_scores);
     fmt::println("result is : {}", sum_result);
+    */
     return 0;
 
   
