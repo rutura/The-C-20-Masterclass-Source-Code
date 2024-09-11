9bbe22c2411a0e0fe2b422df4e30bdaab7a2b403
Daniel Gakwaya <wayadn88@gmail.com>
2024-09-11 16:24:29 +0300

    align chapter 7 on sequences with the slides.

---
+++ 07.Sequences/7.1Std_array_/main.cpp
@@ -0,0 +1,45 @@
+/*
+    . Exploring the std::array collection type in C++.
+    . Goal: 
+        . Show that std::array is one of your best default choices when you need to deal with sequence data in C++.
+    . Benefits: 
+        . Each collection knows its size.
+        . Easy to pass to functions (doesn't need to pass the size as a separate parameter).
+        . Convenient functions to get the front and back elements.
+        . Can be compared using comparison operators (==, !=, <, <=, >, >=).
+        . Can assign a std::array to another std::array.
+        . Can store an std::array in another container.
+        . You don't have to mess with low lever arrays and pointers. 
+
+    . Points to make: 
+        . The differences between at() and operator[].
+        . std::array should be your choice if you know the size of the collection at compile time, and you won't need to change its size.
+    . Build instructions: 
+        . Bare hands with CMake: 
+            . cmake -S . -B build
+            . cmake --build build
+
+        . With CMake presets (VS Code): 
+            . Select configure preset
+            . Select build preset
+            . Use the run and debug buttons: We can debug application right from the start.
+
+        . Can also use presets on the command line.
+    . Dependencies: 
+        . gcc 14 or up (can be official container)
+        . cmake 3.29 or up
+        . ninja 1.11 or up
+        . vcpkg and setting up the VCPKG_ROOT environment variable
+        . gdb any version
+
+    . Philosophy: 
+        . All non module looking code should be hidden away in the utilities module.
+
+*/
+
+// One line comment
+import utilities;
+
+int main() {
+    do_work();
+}
---
+++ 07.Sequences/7.5Fortune_tellter_/main.cpp
@@ -0,0 +1,38 @@
+/*
+    .  Fortune teller example 
+        .Different versions of the fortune teller program. 
+            . From storing predictions as C-style strings to storing them as std::string.
+            . From using std::rand() to using std::mt19937.
+            . And all in betweens. 
+
+    . Build instructions: 
+        . Bare hands with CMake: 
+            . cmake -S . -B build
+            . cmake --build build
+
+        . With CMake presets (VS Code): 
+            . Select configure preset
+            . Select build preset
+            . Use the run and debug buttons: We can debug application right from the start.
+
+        . Can also use presets on the command line.
+    . Dependencies: 
+        . gcc 14 or up (can be official container)
+        . cmake 3.29 or up
+        . ninja 1.11 or up
+        . vcpkg and setting up the VCPKG_ROOT environment variable
+        . gdb any version
+
+    . Philosophy: 
+        . All non module looking code should be hidden away in the utilities module.
+
+*/
+
+// One line comment
+import utilities;
+
+int main() {
+    //This is our best version so far.
+    fortune_teller_version_5();
+    return 0; 
+}
