4c3465bcd82e9cdab536066ac55b72bba390b107
Daniel Gakwaya <wayadn88@gmail.com>
2026-09-23 08:13:59 +0300

    Add lecture 7 in chapter 6

--- 06.Functions/6.7LifetimeAndScope/main.cpp
+++ 06.Functions/6.7LifetimeAndScope/main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include <print>
+
+const int leg_count{ 2 };
+
+
+void call_count() {
+	static int count{ 0 };
+	++count;
+	std::println("Count: {}", count);
+}
+
+
+double multiply_by_2(double a) {
+	double result = a * 2;
+	return result;
+}
+
 
 int main(){
-    std::cout << "Hello, World!" << "\n";
+
+	call_count();
+	call_count();
+
 }
